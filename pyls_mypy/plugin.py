3d2f895d9c66e53f5aa50ca359d9310e18883db9
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-10-06 15:05:06 +0900

    remove debug

--- pyls_mypy/plugin.py
+++ pyls_mypy/plugin.py
@@ -69,9 +69,6 @@ def pyls_lint(config, workspace, document, is_saved):
         return []
 
     report, errors, _ = mypy_api.run(args)
-    with open('/home/hirota/docsim/log.txt', 'a') as fout:
-        fout.write(report)
-        fout.write(errors)
 
     diagnostics = []
     for line in report.splitlines():
